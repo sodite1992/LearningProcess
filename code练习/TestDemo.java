3331326013678c5690c1b4b29260f3b429129912
sodite1992 <Acd.uhuh@outlook.com>
2017-04-19 21:53:26 +0800

    对之前学习内容的回想
    时间太久了，现在重新学习一遍。

---
+++ code练习/TestDemo.java
@@ -0,0 +1,5 @@
+class TestDemo{
+		public static void main(String[] args){
+				System.out.println("I'm learnning Java");
+		}
+}
---
+++ code练习/suanshu.java
@@ -0,0 +1,10 @@
+class suanshu{
+		public static void main(String[] args){
+				int a;
+				int sum=0;
+				for(a=0;a<101;a++){
+						sum=sum+a;
+				}	
+				System.out.println(sum);
+		}
+}
