eb5dadfe418c10e88bb4c2a51af976a296ad48a3
sodite1992 <Acd.uhuh@outlook.com>
2017-04-21 14:20:11 +0800

    各数据类型的输出方式

---
+++ code练习/Practice.java
@@ -0,0 +1,15 @@
+//各数据类型的输出方式
+class Practice{
+	public static void main(String[] args){
+			//输出字符串
+			System.out.println("can");
+			//输出整数
+			System.out.println(1000);
+			//输出小数
+			System.out.println(100.10);
+			//输出字符
+			System.out.println('c');
+			//输出布尔类型
+			System.out.println(true);
+	}	
+}
